3c8c4a9f7e15ac949f4411eae58f494f447a36ac
idkafael <seu-email@exemplo.com>
2025-10-30 23:40:27 -0300

    Melhora tratamento de erros do Facebook Pixel bloqueado por extensões

--- pages/index.js
+++ pages/index.js
@@ -360,20 +360,35 @@ export default function Home() {
       />
       
       {/* Facebook Pixel */}
-      <Script id="fb-pixel" strategy="afterInteractive">
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '${pixelId}');
-          fbq('track', 'PageView');
-        `}
-      </Script>
+      {pixelId && pixelId !== 'SEU_PIXEL_ID_AQUI' && (
+        <Script 
+          id="fb-pixel" 
+          strategy="afterInteractive"
+          onLoad={() => {
+            // Sucesso - Pixel carregado
+          }}
+        >
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.onerror=function(){/* Pixel bloqueado por extensão - normal */};
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            if(s)s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            try {
+              if(typeof fbq !== 'undefined') {
+                fbq('init', '${pixelId}');
+                fbq('track', 'PageView');
+              }
+            } catch(e) {
+              // Silenciar erros se o Pixel estiver bloqueado
+            }
+          `}
+        </Script>
+      )}
       <noscript>
         <img height="1" width="1" style={{display:'none'}}
              src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`} />
